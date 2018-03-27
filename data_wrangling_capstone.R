1f0ca9bef5b7dd162a0d7f3cb0de6989cf81f4f9
KATHLEEN TRACEY <ktrace213@gmail.com>
2018-03-27 19:02:00 -0400

    created separate data frame for pitchers, includes salaries

--- data_wrangling_capstone.R
+++ data_wrangling_capstone.R
@@ -9,13 +9,21 @@ library("tidyr")
 master_data <- Master %>%
   select(playerID, debut, finalGame, throws, bats)
 
-#get debut year (year 1)
+
+#separate debut date into 3 col (debut year, deb_month, deb_day)
 master_data <- master_data %>%
   separate(debut, c("Debut Year", "Deb_Month", "Deb_Day"), sep = "-", remove = TRUE)
+
+#separate date of final game into 3 col (final year, f_month, f_day)
+master_data <- master_data %>%
+  separate(finalGame, c("Final Year", "F_month", "F_day"), sep = "-", remove = TRUE)
+
+
 
 #get relevant fielding data from Fielding data set
 fielding_data <- Fielding %>%
   select(playerID, yearID, POS, InnOuts, PO, A, E)
+
 
 #get relevant batting data from Batting data set
 batting_data <- Batting %>%
@@ -25,12 +33,20 @@ batting_data <- Batting %>%
 pitching_data <- Pitching %>%
   select(playerID, yearID, W, L, G, BAOpp, ERA, BB, SO)
 
-#merge data frames together
+
+#get salary data for each player each year
+player_salary <- Salaries %>%
+  select(playerID, yearID, salary)
+
+#merge data frames together for position players(nonpitchersn ***need to remove pitchers from position player dataframe
 
 all_positions <- left_join(master_data, fielding_data)
 
 all_positions <- left_join(all_positions, batting_data)
 
-all_positions <- left_join(all_positions, pitching_data)
+all_positions <- left_join(all_positions, player_salary)
 
-#separate pitchers out
+#create data frame for pitchers
+all_pitching <- inner_join(master_data, pitching_data)
+
+
