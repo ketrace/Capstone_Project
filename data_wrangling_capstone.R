863ae9202ba912a2599950d79aa76cdecb82cb12
KATHLEEN TRACEY <ktrace213@gmail.com>
2018-03-28 13:25:03 -0400

    updated code to remove pitchers from position player database, calculation of career length included

--- data_wrangling_capstone.R
+++ data_wrangling_capstone.R
@@ -12,12 +12,16 @@ master_data <- Master %>%
 
 #separate debut date into 3 col (debut year, deb_month, deb_day)
 master_data <- master_data %>%
-  separate(debut, c("Debut Year", "Deb_Month", "Deb_Day"), sep = "-", remove = TRUE)
+  separate(debut, c("Debut_Year", "Deb_Month", "Deb_Day"), sep = "-", remove = TRUE)
 
 #separate date of final game into 3 col (final year, f_month, f_day)
 master_data <- master_data %>%
-  separate(finalGame, c("Final Year", "F_month", "F_day"), sep = "-", remove = TRUE)
+  separate(finalGame, c("Final_Year", "F_month", "F_day"), sep = "-", remove = TRUE)
 
+
+#create column for career length (in years)
+master_data <- master_data %>%
+  mutate(Career_Length = as.numeric(Final_Year) - as.numeric(Debut_Year))
 
 
 #get relevant fielding data from Fielding data set
@@ -46,7 +50,15 @@ all_positions <- left_join(all_positions, batting_data)
 
 all_positions <- left_join(all_positions, player_salary)
 
+#create subset of all_positions to remove rows that correspond to pitchers
+all_posplayers <- all_positions %>%
+  subset(POS != "P")
+
 #create data frame for pitchers
 all_pitching <- inner_join(master_data, pitching_data)
 
+#write CSV for all position players
+write.csv(all_posplayers, "all_posplayers.csv")
 
+#write csv for all pitchers
+write.csv(all_pitching, "all_pitching.csv")
